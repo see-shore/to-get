dc2ee18c703713af5fb583ac629f39d8181884cf
James Chung <sunhochung53@gmail.com>
2023-02-14 01:07:59 -0800

    Add Spring boilerplate

---
+++ src/test/java/com/seeshore/toget/ToGetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seeshore.toget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToGetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
