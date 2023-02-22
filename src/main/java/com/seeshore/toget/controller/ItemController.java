564ac1bc8616c2011b0969723158e53d3f8ad984
jdtan <jdtan33@gmail.com>
2023-02-21 20:45:18 -0800

    connect product page to back

--- src/main/java/com/seeshore/toget/controller/ItemController.java
+++ src/main/java/com/seeshore/toget/controller/ItemController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
+@CrossOrigin
 public class ItemController {
 
     @Autowired
