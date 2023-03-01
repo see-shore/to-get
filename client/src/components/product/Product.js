707dc9b9748f37c9e552cf4bcc6de965d4f93c28
James Chung <sunhochung53@gmail.com>
2023-02-28 18:13:19 -0800

    Implement product

--- client/src/components/product/Product.js
+++ client/src/components/product/Product.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IconButton, Grid } from '@mui/material';
+import {
+  Remove as RemoveIcon,
+  Add as AddIcon
+ } from '@mui/icons-material';
 
 import styles from '../../styles/components/Product.json';
 
 function Product() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  const decrement = () => {
+    if (count <= 0) {
+      setCount(0);
+    } else {
+      setCount(count - 1);
+    }
+  }
+
   return (
-    <div style={styles.container}>
-      
-    </div>
+    <Grid container style={styles.container}>
+      <Grid item style={styles.productName}>Product</Grid>
+      <Grid item style={styles.counter}>
+        <IconButton onClick={decrement}>
+          <RemoveIcon />
+        </IconButton>
+        <Grid item style={styles.numberDisplay}>{count}</Grid>
+        <IconButton onClick={increment}>
+          <AddIcon />
+        </IconButton>
+      </Grid>
+    </Grid>
   );
 }
 
