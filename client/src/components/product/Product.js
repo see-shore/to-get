9c268c68209c4b2b1f50946802e5f0d306845383
James Chung <sunhochung53@gmail.com>
2023-02-28 22:43:41 -0800

    Implement product info dialog

--- client/src/components/product/Product.js
+++ client/src/components/product/Product.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
-import { IconButton, Grid } from '@mui/material';
+import {
+  IconButton,
+  Grid,
+  TextField,
+  Dialog,
+  DialogActions,
+  Button,
+  DialogContent,
+  DialogTitle,
+  Box
+} from '@mui/material';
 import {
   Remove as RemoveIcon,
-  Add as AddIcon
- } from '@mui/icons-material';
- import { useDispatch } from 'react-redux';
+  Add as AddIcon,
+  Close as CloseIcon
+} from '@mui/icons-material';
+import { useDispatch } from 'react-redux';
 
 import styles from '../../styles/components/Product.json';
 import { updateCart, removeFromCart } from '../../redux/slices/itemsSlice';
@@ -12,12 +23,13 @@ import { updateCart, removeFromCart } from '../../redux/slices/itemsSlice';
 function Product(props) {
   const dispatch = useDispatch();
   const [count, setCount] = useState(0);
+  const [open, setOpen] = useState(false);
   const { item } = props;
 
   const increment = () => {
     setCount(count + 1);
     dispatch(updateCart({ itemId: item.id, quantity: count + 1 }));
-  }
+  };
 
   const decrement = () => {
     if (count - 1 <= 0) {
@@ -29,27 +41,52 @@ function Product(props) {
         dispatch(updateCart({ itemId: item.id, quantity: count - 1 }));
       }
     }
-  }
+  };
 
   const formatPrice = (price) => {
     const formattedPrice = (price / 100).toFixed(2);
     return `$${formattedPrice}`;
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   }
 
   return (
-    <Grid container sx={styles.container}>
-      <Grid item sx={styles.price}>{formatPrice(item.price)}</Grid>
-      <Grid item sx={styles.productName}>{item.name}</Grid>
-      <Grid item sx={styles.counter}>
-        <IconButton onClick={decrement}>
-          <RemoveIcon />
-        </IconButton>
-        <Grid item sx={styles.numberDisplay}>{count}</Grid>
-        <IconButton onClick={increment}>
-          <AddIcon />
-        </IconButton>
+    <>
+      <Grid container sx={styles.container}>
+        <Grid item sx={{ width: "100%", height: 140, cursor: "pointer"}} onClick={handleOpen}>
+          <Grid item sx={styles.price}>{formatPrice(item.price)}</Grid>
+          <Grid item sx={styles.productName}>{item.name}</Grid>
+        </Grid>
+        <Grid item sx={styles.counter}>
+          <IconButton onClick={decrement}>
+            <RemoveIcon />
+          </IconButton>
+          <Grid item sx={styles.numberDisplay}>{count}</Grid>
+          <IconButton onClick={increment}>
+            <AddIcon />
+          </IconButton>
+        </Grid>
       </Grid>
-    </Grid>
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth='xs' sx={styles.dialog}>
+        <DialogTitle>
+          <Grid sx={styles.dialogTitle}>
+            <p style={styles.cardCopy}>{item.name}</p>
+            <IconButton onClick={handleClose} size='small'>
+              <CloseIcon />
+            </IconButton>
+          </Grid>
+        </DialogTitle>
+        <DialogContent sx={{...styles.dialogContent, display: "flex", justifyContent: "center", alignItems: "center"}}>
+          <p style={styles.cardCopy}>PRODUCT INFO. WHAT GOES HERE?</p>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
 
