9903b8859783cc2e036899635ffb9105a1e92c83
jdtan <jdtan33@gmail.com>
2023-03-09 00:50:07 -0800

    style product page

--- client/src/components/product/Product.js
+++ client/src/components/product/Product.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { IconButton, Grid, Dialog, DialogContent } from '@mui/material';
 import { Remove as RemoveIcon, Add as AddIcon } from '@mui/icons-material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import { useDispatch } from 'react-redux';
 
-import styles from '../../styles/components/Product.json';
 import { updateCart, removeFromCart } from '../../redux/slices/itemsSlice';
+import styles from '../../styles/components/Product.json';
 
 function Product(props) {
   const dispatch = useDispatch();
@@ -47,27 +49,38 @@ function Product(props) {
   };
   return (
     <>
-      <Grid container sx={styles.container}>
-        <Grid item sx={{ width: '100%', height: 140, cursor: 'pointer' }} onClick={handleOpen}>
-          <Grid item sx={styles.price}>
-            {formatPrice(item.price)}
-          </Grid>
-          <Grid item sx={styles.productName}>
-            {item.name}
-          </Grid>
-        </Grid>
-        <Grid item sx={styles.counter}>
-          <IconButton onClick={decrement}>
-            <RemoveIcon />
+      <div style={styles.container}>
+        <div onClick={handleOpen} style={styles.box}>
+          <div style={styles.priceTag}>
+            <p style={styles.priceText}>{formatPrice(item.price)}</p>
+            <p style={styles.priceUnit}>{'/unit'}</p>
+          </div>
+          <div style={styles.crate}>
+            <img style={styles.imageContainer} src={require(`../../images/produce/real_${item.id}.png`)} />
+            <div style={styles.textContainer}>
+              <p style={styles.itemName}>{item.name}</p>
+              <p style={styles.itemType}>{'golden'}</p>
+            </div>
+          </div>
+        </div>
+        <div style={styles.control}>
+          <IconButton
+            onClick={decrement}
+            size={'large'}
+            style={{ ...styles.button, borderRight: '3px dashed #6a5442bd' }}
+          >
+            <FontAwesomeIcon icon={solid('minus')} />
           </IconButton>
-          <Grid item sx={styles.numberDisplay}>
-            {count}
-          </Grid>
-          <IconButton onClick={increment}>
-            <AddIcon />
+          <p style={styles.countText}>{count}</p>
+          <IconButton
+            onClick={increment}
+            size={'large'}
+            style={{ ...styles.button, borderLeft: '3px dashed #6a5442bd' }}
+          >
+            <FontAwesomeIcon icon={solid('plus')} />
           </IconButton>
-        </Grid>
-      </Grid>
+        </div>
+      </div>
       <Dialog open={open} onClose={handleClose} fullWidth maxWidth='xs' sx={styles.dialog}>
         <DialogContent sx={{ ...styles.dialogContent, display: 'flex', justifyContent: 'center' }}>
           <Grid container sx={{ display: 'flex', alignItems: 'center' }}>
